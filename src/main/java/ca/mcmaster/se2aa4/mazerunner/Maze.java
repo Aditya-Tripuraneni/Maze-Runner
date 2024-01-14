b3a699f248f9cbd4587131aab4a072e79ee4a0b9
Aditya-Tripuraneni <aditya.tripuraneni905@gmail.com>
2024-01-14 16:39:48 -0500

    Added skeleton maze class seperate file

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -0,0 +1,105 @@
+public class Maze {
+
+
+    public Maze(String filepath){ 
+        return "";
+    }
+
+
+    /************************************************
+     * @ Method Name: moveLeft()                    
+     * Description: Changes path direction to left. 
+     ************************************************/
+    private String moveLeft(){
+        return "L";
+    }
+
+
+    /************************************************
+     * @ Method Name: moveRight()                    
+     * Description: Changes path direction to right. 
+     ************************************************/
+    private String moveRight(){
+        return "R";
+    }
+
+
+    /************************************************
+     * @ Method Name: moveForward()                    
+     * Description: Changes path direction to forward. 
+     ************************************************/
+    private String moveForward(){
+        return "F";
+    }
+
+
+    /*******************************************************
+     * @ Method Name: cannonicalPath()                    
+     * Description: Returns the cannonical path of the maze. 
+     *******************************************************/
+    private String cannonicalPath(){
+        return "";
+    }
+
+
+    /*******************************************************
+     * @ Method Name: factoredExpressionPath()                    
+     * Description: Returns the factored expression path 
+     *  of the maze so its simplified. 
+     *******************************************************/
+    private String factoredExpressionPath(){
+        return "";
+    }
+
+
+    /*******************************************************
+     * @ Method Name: isWall()                    
+     * Description: Determines if the current input path
+     *  is a wall. 
+     *******************************************************/
+    private boolean isWall(Char inut){
+        return false;
+    }
+
+
+     /*******************************************************
+     * @ Method Name: isExit()                    
+     * Description: Determines if the current input path
+     *  is a exit. 
+     *******************************************************/
+    private boolean isExit(Char input){
+        return false; 
+    }
+
+
+    /*******************************************************
+     * @ Method Name: isEntrance()                    
+     * Description: Determines if the current input path
+     *  is an entrance. 
+     *******************************************************/
+    private isEntrance(Char input){
+        return false; 
+    }
+
+
+    /*******************************************************
+     * @ Method Name: verifyMazePath()                    
+     * Description: Determines if the computed path is a valid 
+     * path for a given maze.
+     *******************************************************/
+    public boolean verifyMazePath(String mazePath, String inputPath){
+        return false;
+    }
+
+
+    /*******************************************************
+     * @ Method Name: solveMaze()                    
+     * Description: Sovles the maze and determins and output
+     * path for the given input maze.
+     *******************************************************/
+    public String solveMaze(Maze maze, String inputPath){
+
+        return ""; 
+    }
+    
+}
