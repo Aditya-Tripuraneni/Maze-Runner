9ece2512cab6c7326f612234978c31aa8db39058
Aditya <aditya.tripuraneni905@gmail.com>
2024-03-28 13:31:28 -0400

    Path display works for tiny and small maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/BreadthFirstSearchSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/BreadthFirstSearchSolver.java
@@ -12,9 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 public class BreadthFirstSearchSolver implements MazeSolver{
 
     private Maze maze; 
