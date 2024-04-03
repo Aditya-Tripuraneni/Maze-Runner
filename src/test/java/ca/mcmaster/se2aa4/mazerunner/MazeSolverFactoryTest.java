e6de5ccdf7ab629b8063c1f0d4445d2da00fa7fe
Aditya <aditya.tripuraneni905@gmail.com>
2024-04-02 21:35:49 -0400

    Added additional tests for MazeSolverFactoryTest

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/MazeSolverFactoryTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/MazeSolverFactoryTest.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileNotFoundException;
@@ -19,7 +20,14 @@ import ca.mcmaster.se2aa4.mazerunner.Paths.Path;
 public class MazeSolverFactoryTest {
     private Path path; 
     private Maze maze; 
+    
 
+    /**
+     * Sets up a maze for testing.
+     *
+     * @throws FileNotFoundException if the maze file is not found.
+     * @throws IOException           if an I/O error occurs while reading the maze file.
+     */
     @BeforeEach
     public void setUpMaze() throws FileNotFoundException, IOException{
         this.path = new Path("./examples/straight.maz.txt");
@@ -27,19 +35,73 @@ public class MazeSolverFactoryTest {
     }
 
 
+    /**
+     * Tests whether the Breadth First Search (BFS) solver created by the MazeSolverFactory
+     * implements the MazeSolver interface.
+     */
     @Test
-    public void testCreateBreadthFirstSearchSolver() throws FileNotFoundException, IOException {
+    public void testBFSImplementsMazeSolver() {
+        MazeSolverFactory factory = new MazeSolverFactory();
+        MazeSolver solver = factory.createSolver("BFS", maze);
+        assertTrue(solver instanceof MazeSolver);
+    }
+
+
+    /**
+     * Tests whether the MazeSolverFactory correctly creates a Breadth First Search (BFS) solver.
+     */
+    @Test
+    public void testCreateBreadthFirstSearchSolver() {
         MazeSolverFactory factory = new MazeSolverFactory();
         MazeSolver solver = factory.createSolver("BFS", maze);
         assertTrue(solver instanceof BreadthFirstSearchSolver);
     }
 
 
+    /**
+     * Tests whether the solver created by the MazeSolverFactory for Breadth First Search (BFS)
+     * algorithm is not an instance of Right Hand Exploration solver.
+     */
     @Test
-    public void testCreateRightHandSolver() throws FileNotFoundException, IOException {
+    public void testBFSSolverNotInstanceRightHandSolver(){
+        MazeSolverFactory factory = new MazeSolverFactory();
+        MazeSolver solver = factory.createSolver("BFS", maze);
+        assertFalse(solver instanceof RightHandExploration);
+    }
+
+    /**
+     * Tests whether the Right Hand Exploration solver created by the MazeSolverFactory
+     * implements the MazeSolver interface.
+     */
+    @Test
+    public void testRightHandImplementsMazeSolver() {
+        MazeSolverFactory factory = new MazeSolverFactory();
+        MazeSolver solver = factory.createSolver("RIGHTHAND", maze);
+        assertTrue(solver instanceof MazeSolver);
+    }
+
+
+    /**
+     * Tests whether the MazeSolverFactory correctly creates a Right Hand Exploration solver.
+     */
+    @Test
+    public void testCreateRightHandSolver() {
         MazeSolverFactory factory = new MazeSolverFactory();
         MazeSolver solver = factory.createSolver("RIGHTHAND", maze);
         assertTrue(solver instanceof RightHandExploration);
     }
+
+
+    /**
+     * Tests whether the solver created by the MazeSolverFactory for Right Hand Exploration
+     * algorithm is not an instance of Breadth First Search (BFS) solver.
+     */
+    @Test
+    public void testRightHandSolverNotInstanceBFS() {
+        MazeSolverFactory factory = new MazeSolverFactory();
+        MazeSolver solver = factory.createSolver("RIGHTHAND", maze);
+        assertFalse(solver instanceof BreadthFirstSearchSolver);
+    }
+
     
 }
